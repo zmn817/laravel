882c3fe4692ec98fc8138d833231cea3c128b1c6
timjuly <timjuly@163.com>
2019-02-22 09:42:58 +0800

    拉钩

---
+++ tests/Unit/Lagou/WebTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ThirtyThree\Tests\Unit\Lagou;
+
+use ThirtyThree\Lagou\Web;
+use ThirtyThree\Tests\TestCase;
+
+class WebTest extends TestCase
+{
+    public function testCompanyDetail()
+    {
+        $wechat = new Web();
+        $company = $wechat->companyInfo(147);
+        $this->assertArrayHasKey('baseInfo', $company);
+    }
+}
